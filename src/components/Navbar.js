c26aa1126823872d52705af96e1d9ddcbffa07fe
imubarak234 <imubarak2424@gmail.com>
2022-06-06 17:19:56 +0100

    dummy verticl navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar = () => {
+  
+  return (
+    <ul class="nav flex-column">
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page" href="#">Active</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+    </li>
+  </ul>
+  )
+}
