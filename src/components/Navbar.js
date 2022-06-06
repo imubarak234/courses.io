ce9a88954103aa09f0b47e07f1e6496faff32fe9
imubarak234 <imubarak2424@gmail.com>
2022-06-06 17:35:26 +0100

    add bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,37 @@
 import React from "react";
+import { Navlink } from 'react-router-dom'
 
-const Navbar = () => {
-  
-  return (
-    <ul class="nav flex-column">
-    <li class="nav-item">
-      <a class="nav-link active" aria-current="page" href="#">Active</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Link</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Link</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-    </li>
-  </ul>
-  )
-}
+const Navbar = () => (
+    <div>
+      <nav>
+        <Navlink>
+          <img src="" alt="" />
+        </Navlink>
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <Navlink class="nav-link active" aria-current="page" href="#">Active</Navlink>
+          </li>
+          <li class="nav-item">
+            <Navlink class="nav-link" href="#">Link</Navlink>
+          </li>
+          <li class="nav-item">
+            <Navlink class="nav-link" href="#">Link</Navlink>
+          </li>
+          <li class="nav-item">
+            <Navlink class="nav-link" href="#">Link</Navlink>
+          </li>
+          <li class="nav-item">
+            <Navlink class="nav-link" href="#">Link</Navlink>
+          </li>
+          <li class="nav-item">
+            <Navlink class="nav-link" href="#">Link</Navlink>
+          </li>
+          <li class="nav-item">
+            <Navlink class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</Navlink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+)
+
+export default Navbar;
