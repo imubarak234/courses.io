fe5043b30c7298507976a79f70888c733bf02023
imubarak234 <imubarak2424@gmail.com>
2022-06-09 14:50:56 +0100

    nav html

--- src/components/Menu/Navbar.js
+++ src/components/Menu/Navbar.js
@@ -2,31 +2,31 @@ import React from 'react';
 import { Navlink } from 'react-router-dom';
 
 const Navbar = () => (
-  <div>
-    <nav>
-      <Navlink>
-        <img src="" alt="" />
+  <div class="h-100" id="side-bar">
+    <nav class="h-100 py-4 d-flex flex-column" >
+      <Navlink class="navbar-brand fw-normal pb-4" href="#">
+        <img src="/images/free-logo-removebg-preview.png" alt="..." class="w-100" />
       </Navlink>
-      <ul className="nav flex-column">
-        <li className="nav-item">
+      <ul class="nav nav-pills nav-fill flex-column ms-3">
+        <li class="nav-item pt-3 pb-2">
           <Navlink class="nav-link active" aria-current="page" href="#">Active</Navlink>
         </li>
-        <li className="nav-item">
+        <li class="nav-item pb-2">
           <Navlink class="nav-link" href="#">Link</Navlink>
         </li>
-        <li className="nav-item">
+        <li class="nav-item pb-2">
           <Navlink class="nav-link" href="#">Link</Navlink>
         </li>
-        <li className="nav-item">
+        <li class="nav-item pb-2">
           <Navlink class="nav-link" href="#">Link</Navlink>
         </li>
-        <li className="nav-item">
+        <li class="nav-item pb-2">
           <Navlink class="nav-link" href="#">Link</Navlink>
         </li>
-        <li className="nav-item">
+        <li class="nav-item pb-2">
           <Navlink class="nav-link" href="#">Link</Navlink>
         </li>
-        <li className="nav-item">
+        <li class="nav-item pb-2">
           <Navlink class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</Navlink>
         </li>
       </ul>
