da67248c326a486d64cd17a278f88e41a4c03b66
imubarak234 <imubarak2424@gmail.com>
2022-06-22 15:16:21 +0100

    configure redux store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux'; 
+import store from './redux/configStore';
 import Login from './components/Login/login';
 import Register from './components/Register/register';
 import Courses from './components/Courses/courses';
@@ -9,12 +11,14 @@ import Courses from './components/Courses/courses';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Courses />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Courses />} />
+        </Routes>
+      </Router>
+    </Provider>
   </React.StrictMode>,
 );
---
+++ src/redux/configStore.js
@@ -0,0 +1,12 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import coursesSelectReducer from "./courses/coursesSelect";
+
+const store = configureStore({
+  reducer: {
+    select: coursesSelectReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
+
+export default store;
