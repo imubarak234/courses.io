80a26d5cffb1a28171f964fad3bd2bf056ed4abc
imubarak234 <imubarak2424@gmail.com>
2022-08-02 19:31:57 +0100

    show courses

---
+++ src/components/Courses/showCourse.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Menu/Navbar';
+
+
+const ShowCourse = () => {
+  return (
+    <div className="d-flex h-100">
+      <Navbar />
+    </div>
+  )
+}
+
+export default ShowCourse;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import store from './redux/configStore';
 import Login from './components/Login/login';
 import Register from './components/Register/register';
 import Courses from './components/Courses/coursesContainer';
+import ShowCourse from './components/Courses/showCourse';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,6 +17,7 @@ root.render(
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/show" element={<ShowCourse />} />
           <Route path="/" element={<Courses />} />
         </Routes>
       </Router>
