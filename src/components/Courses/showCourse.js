45c67ae0d52054850922d568f33e281f18a265aa
imubarak234 <imubarak2424@gmail.com>
2022-08-04 17:58:21 +0100

    sign out

--- src/components/Courses/showCourse.js
+++ src/components/Courses/showCourse.js
@@ -1,3 +1,6 @@
+/* eslint-disable global-require */
+/* eslint-disable max-len */
+
 import React from 'react';
 import Navbar from '../Menu/Navbar';
 import './courses.css';
@@ -8,7 +11,7 @@ const ShowCourse = () => (
 
     <div className="d-flex justify-content-between" id="second-half">
       <div className="h-100 w-75 d-flex justify-content-center align-content-center">
-        <img src="./images/undraw_Static_website_re_x70h-removebg-preview.png" alt="" className="img-fluid" />
+        <img src={require('../images/undraw_Content_team_re_6rlg.png')} alt="" className="img-fluid" />
       </div>
       <div className=" h-100 w-25 me-5">
         <h2 className="h2 p-2 mb-2">Title of Course</h2>
@@ -32,11 +35,11 @@ const ShowCourse = () => (
           <p className="p-2 mb-2">Description Description Description Description Description Description</p>
         </span>
 
-        <button type="button" className="btn btn-outline-success">
+        <a href="/" className="btn btn-outline-success">
           <i className="bi bi-card-checklist px-2" />
           Reserve Course
           <i className="bi bi-arrow-right-circle px-2" />
-        </button>
+        </a>
       </div>
     </div>
   </div>
